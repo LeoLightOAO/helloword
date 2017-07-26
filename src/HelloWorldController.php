109b8ffdd5bb6413f781c640b683bc1dd118dcaa
Light <googfrank@gmail.com>
2017-07-27 02:56:05 +0800

    Initial commit.

---
+++ src/HelloWorldController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Oomusou\HelloWorld;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HelloWorldController extends Controller
+{
+    //
+    public function index()
+    {
+        $message = 'Hello World';
+        return view('HelloWorld::welcome', compact('message'));
+    }
+}
